061f0669de589d245838971f7fcebca324c7c834
Timothy Laurent <timothyjlaurent@gmail.com>
2013-08-13 23:28:15 -0700

    finished the basic program

--- ftclient.py
+++ ftclient.py
@@ -1,5 +1,26 @@
 from socket import *
 import sys
+import os
+# by Timothy Laurent
+# Networking
+# 8-13-2013
+
+# usage : python ftclient.py
+## Ft client initiates a ftclient object whick creates the
+# conncetion socket and connects to the server.
+# Upon connection, the client calls listenDataSocket() to open a 
+# dsock datasocket class attribute listening on the dataport, 30020, and then sends
+# the 'ready' signal to the server. The client calls a commandSession() logic
+# method in a forever loop. In the commandSession, the user is prompted
+# to input a command. The possible commands are 'list', 'get <file>', or quit.
+# The client send all entered commends to the server and displays the server's
+# response. If the response is 'quit' the client closes the sockets and 
+# quits. If the recieves the "getting <filename> size x" string, the client send a confirmation 
+# message "transfer" and then copys the file from the sdata socket to the current directory. 
+# if the there is another file by that name in the dircectory, the user will be 
+# asked whether to overwrite or cancel the transfer.
+
+
 
 class ftclient:
 	'''This client makes a command connection to 
@@ -9,38 +30,79 @@ class ftclient:
 	'''
 
 	def __init__(self, addr='localhost', port= 30021 ):
+		'''Connects to server then sets up a listening 
+		socket for the data connection before signalling
+		to the server that it is ready to initialize the data
+		connection
+		'''
 		self.commSocket  =socket(AF_INET, SOCK_STREAM)
 		self.commSocket.connect((addr, port))
-		self.commandSession();
+		self.listenDataSocket()
+		self.commSocket.send("ready")
+		self.dsock, self.daddr = self.dlsock.accept()
+		self.connection = 1
+		while self.connection == 1:
+			self.commandSession();
 
+	def listenDataSocket(self, port = 30020):
+		'''opens a listening socket
+		'''
+		self.dlsock = socket(AF_INET, SOCK_STREAM)
+		self.dlsock.bind(('', port))
+		self.dlsock.listen(1)
+		
 	def commandSession(self):
-		# while(1):
-		while self.commSocket:
-			comm = raw_input('Enter Command\n>')
-			self.commSocket.send(comm)
-			resp = self.commSocket.recv(4096)
-			if resp == "quit":
-				self.commSocket.close()
-				self.commSocket = 0
-				print "connection closed"
-				# sys.exit()
-			elif resp.split()[0] == "incomingFileSize":
-				print 'from server:', resp
-			else:
-				print resp
+		'''this is the command session interface
+		'''
+		comm = raw_input('Enter Command\n>')
+		if comm == "":
+			comm = "emptyString" 
 
-	def openTransferSocket( self, size, port,file ):
-		self.transferSocket = socket(AF_INET, SOCK_STREAM)
-		self.transferSocket.bind(("", port))
-		self.transferSocket.listen(1)
+		self.commSocket.send(comm)
+		if self.connection ==1 :
+			resp = self.commSocket.recv(4096)
+		# print "resp " + resp
+		if resp == "quit":
+			self.commSocket.close()
+			self.connection = 0
+			self.dsock.close()
+			print "connection closed"
+			sys.exit()
+		
+		elif len(resp) > 1 and resp.split()[0] == "Getting" and resp.split()[2] == "size":
+			# print 'from server:', resp
+			self.commSocket.send("transfer")
+			file = resp.split()[1]
+			# print "file\t"+file
+			size = resp.split()[3]
+			# print "size\t"+ str(size)
+			self.receiveFile(file , size)
+		else:
+			print resp
+		
+
+
+	def receiveFile ( self , file, size):
+		'''this recieves the file and checks for file duplication 
+		allowin the user to cancel duplicate saves
+		'''
 		print "ready to receive file"
 		msg = ''
+		size = int(size)
 		while len(msg) < size:
-			chunk = self.sock.recv(size-len(msg))
+			chunk = self.dsock.recv(size-len(msg))
 			if chunk == '':
 				raise RuntimeError("socket connection broken")
 			msg = msg + chunk
-
+		# print msg
+		if os.path.isfile(file):
+			res= raw_input(file + " exists. Overwrite??\n\n(y/n):  ")
+			if res != 'y':
+				print "Cancelled file save"
+				return
+			with open(file, 'w') as out:
+				out.write(msg)
+			print "Transfer Complete!!!"
 
 if __name__=="__main__":
 	client = ftclient()
--- ftserve.py
+++ ftserve.py
@@ -1,69 +1,133 @@
 from socket import *
 import os
+# by Timothy Laurent
+# Networking
+# 8-13-2013
 
-class ftserver:
-    '''This class creates a command connection
-    for the server. It allows the server to 
-    respond to commands and initiated a new 
-    file transfer socket when asked to do so.
-    '''
+# usage: python ftserve.py 
+# this program starts and initiates witht he creation of a listenSocket object listening on port 30021,
+# when the remoteclient connects a commSocket object is created of for the new connection,
+# the server waits on a "ready" signal from 
+# the client, indicating that the client is listening on the data socket 30020
+# The server then creates a new datasocket object socket to the client.
+# The server waits for command on the comm socket and responds with the 
+# different methods list(), errorMsg(), and get(); get() calls the 
+# dataSocket push() command to read and send the file. Gets waits on confirmation
+# before sending the file. If the server gets the quit command the commSocket Class __init__
+# is returned from which returns the server to waitin for another connection.
+# code for thei project adapted from the code examples in the lecture and from 
+# the Python socket howto http://docs.python.org/2/howto/sockets.html
 
-    def __init__(self, sock=None, port=30021):
-        if sock is None:
-            self.sock = socket(AF_INET, SOCK_STREAM)
-        else:
-            self.sock = sock
-        self.port = port
-        self.socklisten()
+class listenSocket:
+	'''Class for making new listening sockets
+	'''
+	
+	def __init__(self, sock=None, port=30021):
+		self.sock = socket(AF_INET, SOCK_STREAM)
+		self.port = port
+		self.sock.bind(('',self.port))
+		self.sock.listen(1)
+		print "server is listening"
+		while 1:
+			commSock, addr = self.sock.accept()
+			cs = commSocket(commSock, addr)
+			print "The server is ready to receive Commands"
 
-    def socklisten(self):
-        self.sock.bind(('',self.port))
-        self.sock.listen(1)
-        print "The server is ready to receive"
-        self.commSocket = 1
-        while(self.commSocket):
-            self.commSocket, addr = self.sock.accept();
-            while(self.commSocket):
-                command = (self.commSocket.recv(4096) ).strip()
-                print command
-                if command == "list":
-                    msg = self.list()
-                    self.commSocket.send("\nDirectory Listings:\n\n" + msg)
-                elif command.split()[0] == "get" and len(command.split()) == 2:
-                    rfile = command.split()[1]
-                    print "rfile = " + rfile
-                    self.get( rfile)
-                elif command == "quit":
-                    self.quit()
-                else:
-                    msg = "Error Enter list, get <filename>, or quit"
-                    self.commSocket.send(msg)
-        
+class commSocket:
+	'''class for making communication sockets
+	'''
+	def __init__(self, commsock, addr ):
+		dataport = 30020
+		self.commsock = commsock
+		if (self.commsock.recv(4096) == "ready"):
+			self.ds = dataSocket(addr, dataport)
+		else:
+			self.commsock.send("connection error")
+		while 1:
+			# logic
+			self.command = (self.commsock.recv(4096) ).strip()
+			print "command\t" +self.command
+			if self.command == "":
+				print "empty String"
+				self.errorMsg()
+			elif self.command == "list":
+				self.list()
+			elif len(self.command.split()) >1 and self.command.split()[0] == "get" and len(self.command.split()) == 2:
+				self.getReq = self.command
+				self.get(self.command.split()[1])
+			elif self.command == "quit":
+				self.commsock.send("quit")
+				return
+			else:
+				self.errorMsg()
 
-    def list(self):
-        files = os.listdir(os.curdir)
-        msg = ""
-        for file in files:
-            msg = msg + file + "\n"
-        return msg
+			print "end of logic loop"
 
-    def quit(self):
-        self.commSocket.send("quit")
-        self.commSocket = 0
+	def errorMsg(self):
+		''' sends error message to the client
+		'''
+		msg = "Error Enter list, get <filename>, or quit"
+		self.commsock.send(msg)
 
-    def get(self, file):
-       
-        try :
-            size = os.path.getsize(file)
-        except os.error:
-            self.commSocket.send("Error File Not Found")
-        else:
-            transferPort = 30021
-            self.commSocket.send("incomingFileSize " + str(size) + " port " + str(transferPort + " filename " + file))
-            openTransferSocket(self, size, 10044,file)
+	def get (self, file ):
+		'''method to check if the file exists, get the size of the
+		file and have the datasocket object read the file and 
+		send to client. waits for the 'transfer' command from the 
+		client indicating that the client is ready to receive the file
+		before calling the push command.
+		'''
+		print "in get command"
+		try :
+			size = os.path.getsize(file)
+		except os.error:
+			self.commock.send("Error File Not Found")
+		else:
+			self.commsock.send("Getting " + self.getReq.split()[1] + " size " + str(size))
+			if (self.commsock.recv(4096) == "transfer"):
+				self.ds.push(self.getReq.split()[1], size)
+			else :
+				self.commsock.send("connection Error")
 
-    def PushFile(self, size, port, file )
+	def list (self):
+		'''handles the "list" command and sends
+		the files in the current director to the 
+		client
+		'''
+		files = os.listdir(os.curdir)
+		msg = ""
+		for file in files:
+			msg = msg + file + "\n"
+		self.commsock.send(msg)
 
-if __name__=="__main__":
-    server = ftserver()
+class dataSocket:
+	'''class for making data connections
+	'''
+	def __init__(self, addr, dataport ):
+		print "addr " + str(addr)
+		datasock = socket(AF_INET, SOCK_STREAM)
+		datasock.connect((addr[0], dataport))
+		self.datasock = datasock
+
+	def push(self, file, size):
+		'''reads and pushes the file to the client
+		'''
+		print "in push command"
+		filestr = open(file, 'r').read()
+		totalsent= 0 
+		while totalsent < size:
+			sent = self.datasock.send(filestr[totalsent:])
+			print "Bytes sent\t" + str(sent)
+			if sent == 0:
+				raise RuntimeError("socket connection broken")
+			totalsent = totalsent + sent
+
+if __name__ == "__main__":
+	ls = listenSocket()
+
+
+
+
+
+
+
 
